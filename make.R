b418c7e4dd6298a8768ce4329166307be3341e59
Randy <rsm5139@scrim-presenter.local>
2015-12-21 16:51:49 -0500

    Progress

--- make.R
+++ make.R
@@ -1,9 +1,9 @@
 #!/usr/local/bin/Rscript
-# convert_all.R
+# make.R
 # Randy Miller
 # rsm5139@psu.edu
 #
-# Last updated on: 17 December 2015
+# Last updated on: 21 December 2015
 #
 # This script takes all of the textbook files written in R Markdown and
 # converts them to PDF and HTML files. 
@@ -13,9 +13,36 @@
 # Usage
 # The purpose of this script is to allow users without a local copy of 
 # RStudio to convert all of the R Mardown files. Therefore, it is designed 
-# to be executed in a Linux environment.
+# to be executed in a Linux environment, or in the R gui.
+
+# Required packages: rmarkdown
+# Other required software: pandoc, latex
+
 # From the command line, type the following and press 'Enter':
 
-# ./convert_all.R
+# ./make.R
 
-rmarkdown::render("summary.Rmd", "all")
+# R Markdown library
+library("rmarkdown")
+
+dir.create("pdf")
+
+# copy cover photo to pdf folder
+file.copy("source/figures/cover_with_alexander.pdf", "pdf")
+
+render("source/cover.Rmd", "pdf_document", output_dir = "pdf")
+render("source/summary.Rmd", "pdf_document", output_dir = "pdf")
+render("source/chap_list.Rmd", "pdf_document", output_dir = "pdf")
+render("source/contrib_bios.Rmd", "pdf_document", output_dir = "pdf")
+render("source/acknowledgements.Rmd", "pdf_document", output_dir = "pdf")
+render("source/intro.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab0_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab1_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab2_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab3_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab4_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab5_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab6_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab7_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/lab8_doc.Rmd", "pdf_document", output_dir = "pdf")
+render("source/license.Rmd", "pdf_document", output_dir = "pdf")
