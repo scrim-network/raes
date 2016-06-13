fd1d03adc36e2cfadc5f4adff97d38c226002acb
climateguy <user@email.fake>
2016-06-13 13:31:35 -0400

    Removed redundant lines

--- make.R
+++ make.R
@@ -50,7 +50,5 @@ file.copy("src/sample_scripts/lab3_sample.R", "scripts")
 file.copy("src/sample_scripts/lab4_sample.R", "scripts")
 file.copy("src/sample_scripts/lab5_sample.R", "scripts")
 file.copy("src/sample_scripts/lab8_sample.R", "scripts")
-file.copy("src/sample_scripts/LICENSE_GPLv3.pdf", "scripts")
-file.copy("src/sample_scripts/README.pdf", "scripts")
 
 system("gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -sOutputFile=raes_v1p2.pdf pdf/cover_with_alexander.pdf pdf/title_page.pdf pdf/main.pdf")
